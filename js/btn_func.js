0ac01931a7c6b243bea0b5d77177c32f0805aa19
Black Cat <papaiabhi70599@gmail.com>
2020-05-07 16:59:00 +0530

    my portfolio
    hello

---
+++ js/btn_func.js
@@ -0,0 +1,49 @@
+document.getElementById("ecmt_page").onclick = function () {
+  location.href = "ECMT/ecmt.html";
+};
+document.getElementById("ecpt_page").onclick = function () {
+  location.href = "ECPT/ecpt.html";
+};
+document.getElementById("c_tuts").onclick = function () {
+  location.href = "cplus/1_about_tutorial.html";
+};
+document.getElementById("cplus_tuts").onclick = function () {
+  location.href = "cplus/1_about_tutorial.html";
+}; 
+document.getElementById("python_tuts").onclick = function () {
+  location.href = "cplus/1_about_tutorial.html";
+}; 
+document.getElementById("html_tuts").onclick = function () {
+  location.href = "cplus/1_about_tutorial.html";
+}; 
+document.getElementById("java_tuts").onclick = function () {
+  location.href = "java/1_about_tutorial.html";
+}; 
+document.getElementById("css_tuts").onclick = function () {
+  location.href = "cplus/1_about_tutorial.html";
+}; 
+document.getElementById("php_tuts").onclick = function () {
+  location.href = "cplus/1_about_tutorial.html";
+}; 
+document.getElementById("more_course").onclick = function () {
+  location.href = "course_page/courses.html";
+};
+document.getElementById("c_test").onclick = function () {
+  location.href = "form/register.html";
+}; 
+document.getElementById("cplus_test").onclick = function () {
+  location.href = "form/register.html";
+}; 
+document.getElementById("python_test").onclick = function () {
+  location.href = "form/register.html";
+};
+
+// document.getElementById("about_us").onclick = function () {
+//   location.href = "error_pages/error-403.html";
+// }; 
+// document.getElementById("Vission_mission").onclick = function () {
+//   location.href = "ECMT/vision_mission.html";
+// };
+// document.getElementById("python_test").onclick = function () {
+//   location.href = "form/register.html";
+// };
